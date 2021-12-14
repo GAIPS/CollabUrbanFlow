cc45f9a013c6ea214f24cacc6e9fcc9af762af73
Guilherme Varela <guilhermevarela@protonmail.com>
2021-12-14 08:47:06 -0300

    Remove marlin agent-system

--- agents/__init__.py
+++ agents/__init__.py
@@ -3,12 +3,10 @@ from pytorch_lightning import seed_everything
 import numpy as np
 
 from agents.actor_critic import ACAT
-from agents.marlin import MARLIN
 from agents import dqn, gat
 
 def load_agent(env, agent_type, chkpt_dir_path, chkpt_num, rollout_time, network):
     if agent_type == 'ACAT': return ACAT.load_checkpoint(chkpt_dir_path, chkpt_num), None
-    if agent_type == 'MARLIN': return MARLIN.load_checkpoint(chkpt_dir_path, chkpt_num), None
     if agent_type == 'DQN': return dqn.load_checkpoint(env, chkpt_dir_path, rollout_time, network)
     if agent_type == 'GAT': return gat.load_checkpoint(env, chkpt_dir_path, rollout_time, network)
     raise ValueError(f'{agent_type} not defined.')
@@ -20,10 +18,6 @@ def get_agent(env, train_args):
     if agent_type == 'ACAT':
         return ACAT(env.phases, train_args.epsilon_init,
                     train_args.epsilon_final, train_args.epsilon_timesteps)
-    if agent_type == 'MARLIN':
-        return MARLIN(env.phases, train_args.epsilon_init,
-                      train_args.epsilon_final, train_args.epsilon_timesteps, env.network)
-
     if agent_type in ('DQN', 'GAT'):
         # TODO: Hparameters becomes model_args
         device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
